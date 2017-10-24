019b0e8de4e143bade2118e438c4f1536d89a0b9
pereztjacob <pereztjacob@gmail.com>
2017-10-24 14:22:43 -0700

    forgot to do this earlier. it all works here.

---
+++ app.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var answerOne = prompt('Is Portland any fun?');
+if(answerOne.toLowerCase === 'yes' || answerOne.toLowerCase === 'y'){
+    alert('Yeah, it can be pretty fun.');
+}else{
+    alert('I think it is sometimes.');
+}
+var answerTwo = prompt('Is Jacob allowed to attend the fun things in Portland?');
+if(answerTwo.toLowerCase === 'yes' || answerTwo.toLowerCase === 'y'){
+    alert("No. He's too young to get in.");
+}else{
+    alert(":(");
+}
+var answerThree = prompt('Does Jacob have houseplants?');
+if(answerThree.toLowerCase === 'yes' || answerThree.toLowerCase === 'y'){
+    alert("You're right, he's got one.");
+}else{
+    alert('No, he has one.');
+}
+var answerFour = prompt('Question 4?');
+if(answerFour.toLowerCase === '#'){
+    alert('');
+}else{
+    alert('');
+}
+var answerFive = prompt('Question 5?');
+if(answerFive.toLowerCase === '#'){
+    alert('');
+}else{
+    alert('');
+}
