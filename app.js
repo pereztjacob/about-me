9dd0e0efd851b5f9f8cbf4154cefe42357883cf7
pereztjacob <pereztjacob@gmail.com>
2017-10-24 14:43:02 -0700

    permissible commit w/out stretch goals

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
 "use strict";
 
 var answerOne = prompt('Is Portland any fun?');
-if(answerOne.toLowerCase === 'yes' || answerOne.toLowerCase === 'y'){
+if(answerOne.toLowerCase() === 'yes' || answerOne.toLowerCase() === 'y'){
     alert('Yeah, it can be pretty fun.');
 }else{
     alert('I think it is sometimes.');
 }
+console.log(answerOne);
 var answerTwo = prompt('Is Jacob allowed to attend the fun things in Portland?');
-if(answerTwo.toLowerCase === 'yes' || answerTwo.toLowerCase === 'y'){
+if(answerTwo.toLowerCase() === 'yes' || answerTwo.toLowerCase() === 'y'){
     alert("No. He's too young to get in.");
 }else{
     alert(":(");
 }
 var answerThree = prompt('Does Jacob have houseplants?');
-if(answerThree.toLowerCase === 'yes' || answerThree.toLowerCase === 'y'){
+if(answerThree.toLowerCase() === 'yes' || answerThree.toLowerCase() === 'y'){
     alert("You're right, he's got one.");
 }else{
     alert('No, he has one.');
 }
-var answerFour = prompt('Question 4?');
-if(answerFour.toLowerCase === '#'){
-    alert('');
+var answerFour = prompt('Can Jacob watch four horror movies in a row?');
+if(answerFour.toLowerCase() === 'yes' || answerFour.toLowerCase() === 'y'){
+    alert("He hasn't proven that yet but he will keep trying.");
 }else{
-    alert('');
+    alert("You're right but it's a work in progress.");
 }
-var answerFive = prompt('Question 5?');
-if(answerFive.toLowerCase === '#'){
-    alert('');
+var answerFive = prompt("What's Jacob's favorite book store in Portland?");
+if(answerFive.toLowerCase() === "Powell's" || answerFive.toLowerCase() === "Powell's City of Books"
+    || answerFive.toLowerCase() === "powells"){
+    alert('Yes.');
 }else{
-    alert('');
+    alert('No.');
 }
